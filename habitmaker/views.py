468c294c3c6ac811f79bd696b6a0d6c6b09a7198
pptnz <eye2988@gmail.com>
2018-05-30 00:35:58 +0900

    Connect habit database to the view

--- habitmaker/views.py
+++ habitmaker/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Habit
 
 
 # Create your views here.
 def habit_list(request):
-    return render(request, 'habitmaker/habit_list.html', {})
+    # After implement Sign-in
+    # habits = Habit.objects.filter(user=request.user).order_by('created_date')
+    habits = Habit.objects.all()
+    return render(request, 'habitmaker/habit_list.html', {'habits': habits})
