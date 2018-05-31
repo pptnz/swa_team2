f2fb32ae8033aa62fffbc4e6d88ac4f9be9b163f
Jonghoon Won <baryberri@icloud.com>
2018-05-31 22:45:21 +0900

    Change models

--- habitmaker/models.py
+++ habitmaker/models.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 
 
 class Habit(models.Model):
-    user = models.ForeignKey('auth.User')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     created_date = models.DateTimeField(default=timezone.now)
