dc57c07de193ad6305c81b764832983fbff046a1
Jonghoon Won <baryberri@icloud.com>
2018-06-01 16:10:35 +0900

    Fix errata: django_users to django_user

--- signin/models.py
+++ signin/models.py
@@ -6,7 +6,7 @@ class CustomUser(models.Model):
     """
     Custom User model that includes 'is email authenticated' field.
     """
-    django_users = models.OneToOneField(User, on_delete=models.CASCADE, related_name='custom_user')
+    django_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='custom_user')
     is_email_authenticated = models.BooleanField(default=False)
 
     def authenticate_email(self):
