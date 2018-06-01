8c00a541551417e8e58ba65b491b28c9ca37cf89
hojunroks <hojunroks@snu.ac.kr>
2018-06-01 11:26:47 +0900

    Add files via upload

--- todo/models.py
+++ todo/models.py
@@ -5,6 +5,6 @@ class ToDo(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     repetition = models.BooleanField()
-    repetition_period = models.DurationField
+    repetition_period = models.DurationField()
 
 # Create your models here.
