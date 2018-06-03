6d490c00f713ac86d9ecbd7b0b51fdc0ef9416a4
Jonghoon Won <baryberri@icloud.com>
2018-06-03 23:27:01 +0900

    Add min_length condition to username and password at sign-in stage

--- signin/forms.py
+++ signin/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SignInForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=15)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput, max_length=30)
+    username = forms.CharField(label='Username', min_length=3, max_length=15)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput, min_length=8, max_length=30)
 
