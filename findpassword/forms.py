2758328a9979c52c8dbc0df840de501e5df39aa6
Jonghoon Won <baryberri@icloud.com>
2018-06-05 01:16:18 +0900

    Implement very simple find_password page logic

---
+++ findpassword/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class FindPasswordForm(forms.Form):
+    username = forms.CharField(label='Username', min_length=3, max_length=15)
+    email = forms.EmailField(label='Email (Optional)', required=False)
--- findpassword/views.py
+++ findpassword/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import FindPasswordForm
 
-# Create your views here.
+
+def find_password_page(request):
+    find_password_form = FindPasswordForm()
+    return render(request, 'findpassword/findpassword.html', {'find_password_form': find_password_form})
