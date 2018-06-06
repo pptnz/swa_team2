2a0026fb4eb7c217be165959b3d8facc9f024ce9
pptnz <eye2988@gmail.com>
2018-06-07 00:28:40 +0900

    Implement Todo

--- todo/models.py
+++ todo/models.py
@@ -1,14 +1,14 @@
+from django.contrib.admin.widgets import AdminDateWidget, AdminSplitDateTime
 from django.db import models
-from django.utils import timezone
+from signin.models import CustomUser
+
 
 class ToDo(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    repetition = models.BooleanField()
-    repetition_period = models.IntegerField()
-    repeat_until = models.DateField()
-# Create your models here.
+    repetition = models.BooleanField(default=False)
+    repeat_start = models.DateField(null=True)
+    repeat_end = models.DateField(null=True)
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,39 @@
+from django.http import HttpResponseRedirect, HT
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+
+from signin.models import CustomUser
 from .models import ToDo
 from .forms import ToDoForm
 
 # Create your views here.
 def todo(request):
-    form = ToDoForm()
-    return render(request, 'todo/todo.html', {'form': form})
+    pass
 
-def add_new(request):
-    form = ToDoForm()
-    return render(request, 'todo_add/todo_add.html', {'form': form})
+
+def post_todo(request):
+    if request.method == 'GET':
+        form = ToDoForm()
+        return render(request, 'todo/todo.html', {'form': form})
+
+    if request.method == 'POST':
+        form = ToDoForm(request.POST)
+        if form.is_valid():
+            # user = request.user
+            user = CustomUser.objects.get(pk=1)
+            title = form.cleaned_data['title']
+            date = form.cleaned_data['date']
+            start_time = form.cleaned_data['start_time']
+            end_time = form.cleaned_data['end_time']
+            repetition = form.cleaned_data['repetition']
+            repetition_start = form.cleaned_data['repetition_start']
+            repeat_end = form.cleaned_data['repeat_end']
+
+            todo = ToDo(user=user, title=title, date=date, start_time=start_time,
+                        end_time=end_time, repetition=repetition,
+                        repetition_start=repetition_start, repeat_end=repeat_end)
+            todo.save()
+            return HttpResponseRedirect('/todo/')
+
+        # Form is not valid
+        return render(request, 'todo/post_todo.html', {'form': form})
